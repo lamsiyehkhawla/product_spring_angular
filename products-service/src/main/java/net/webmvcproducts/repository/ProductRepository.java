538b63ab5629989bd9329a92d113028e907b0336
khawla lamsiyeh <61538329+khawlalam@users.noreply.github.com>
2025-07-27 16:24:07 +0000

    Re-added products-service as a normal directory

---
+++ products-service/src/main/java/net/webmvcproducts/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package net.webmvcproducts.repository;
+
+import net.webmvcproducts.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
